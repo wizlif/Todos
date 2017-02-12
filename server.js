b3169e2a56a4879a7d35c08ca6fcb39587f04123
root <wizlif.144@gmail.com>
2017-02-12 13:26:35 +0300

    Setting up client side of MEAN App

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ var app=express();
 //Views
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.engine('html', require('ejs').renderFile);
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'client/')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
